09869640a0c4ae9e0b23934b4eedfb8b14ed7055
Silvana Mayra Saravia <109870048+SMSnice@users.noreply.github.com>
2023-06-20 11:49:56 -0300

    Actualizaciones
    Agregado de Clase, Objeto, funciones de orden superior, etc.

--- main.js
+++ main.js
@@ -2,16 +2,57 @@
 const PRECIO_MEMBRILLO = 60;
 const PRECIO_CHIPSCHOCO = 80;
 const PRECIO_OREO = 95;
-const SALIR = 4;
+const SALIR = 6;
+const ArrayGalletitas = [];
+const ArrayCarrito = [];
+let ID = 1;
 
-let marca_galletitas = parseInt(prompt("Ingrese opción de galletitas 1,2,3 o 4 para SALIR, \n '1' (100 gr GALLETITAS CON MEMBRILLO/PEPAS $60), \n '2'(100 gr GALLETITAS CON CHIPS DE CHOCOLATE $80), \n '3' (100 gr GALLETITAS OREO $95), \n '4' SALIR"));
-let cantidad_paquetes = parseInt(prompt("Ingrese cantidad de paquetes"));
+
+class Galletitas{
+        constructor(id, categoria, precio, descripcion){
+                this.id = id;
+                this.categoria = categoria;
+                this.precio = precio;
+                this.descripcion = descripcion;
+                }
+}
+
+const galletita1 = new Galletitas(ID, "Galletitas de membrillo", 60, "Paquete de 100 gr. de galletitas de membrillo");
+ArrayGalletitas.push(galletita1);
+
+const galletita2 = new Galletitas(ID, "Galletitas con chips de chocolate", 80, "Paquete de 100 gr. de galletitas con chips de chocolate");
+ArrayGalletitas.push(galletita2);
+
+const galletita3 = new Galletitas(ID, "Galletitas tipo Oreo", 80, "Paquete de 100 gr. de galletitas tipo Oreo");
+ArrayGalletitas.push(galletita3);
+
+const AgregarCarritoCompras = (categoria) =>{
+        ArrayGalletitas.filter((el)=> el.categoria === categoria); // el filter, retorna un array con los elementos cuya condicion sean verdaderas
+        let salida = '';
+        (ArrayGalletitas.filter((el)=> el.categoria === categoria)).forEach((el) =>{
+        salida += "\n" + el.id + "-Galletitas: " + el.categoria + " " + "\nPrecio: " + el.precio
+        });
+        const id = parseInt(prompt(salida + "\n" + "Ingrese producto para agregar al carrito"));
+        const productoEcontrado = ArrayGalletitas.find(el => el.id === id);
+        ArrayGalletitas.push(productoEcontrado);
+        }
+
+const MostrarCarritoCompras = () => {
+        let salida = 'Listado del Carrito de Compras \n';
+        ArrayGalletitas.forEach((el) =>{
+                salida += "\n" + "Galletitas: " + el.categoria + " " + "\nPrecio: " + el.precio});
+        const total = ArrayGalletitas.reduce((acumulador,element) => acumulador + element.precio,0)
+        salida += "\nEl total es: " + total
+        alert(salida);
+}
+
+
+let marca_galletitas = parseInt(prompt("Ingrese categoría de galletitas 1,2,3 o 4 para SALIR, \n '1' (100 gr GALLETITAS CON MEMBRILLO/PEPAS $60) \n '2'(100 gr GALLETITAS CON CHIPS DE CHOCOLATE $80) \n '3' (100 gr GALLETITAS OREO $95) \n '4' Agregar al Carrito de Compras  \n '5' Ver Carrito de Compras  \n '6' SALIR"));
 
 const calcular_precio_total = (precio, cantidad_paquetes) => {
     const precio_total = precio * cantidad_paquetes;
-    alert("El precio total es: $"+ precio_total);
-}
-
+    alert("\nEl precio total es: $"+ precio_total);
+  }
 
 while(marca_galletitas != SALIR ){
 let cantidad_paquetes = parseInt(prompt("Ingrese cantidad de paquetes"));
@@ -23,12 +64,18 @@ let cantidad_paquetes = parseInt(prompt("Ingrese cantidad de paquetes"));
                 break;
         case 3: calcular_precio_total(PRECIO_OREO, cantidad_paquetes);
                 break;
+        case 4: AgregarCarritoCompras();
+                break;
+        case 5: MostrarCarritoCompras();
+                break;
+        case 6: 
+                break;
         default:
-            alert("ERROR. Ingrese sólo 1, 2 o 3");
+            alert("ERROR. Ingrese sólo 1, 2, 3, 4, 5");
             break;
     }
 
-    marca_galletitas = parseInt(prompt("Ingrese opción de galletitas 1,2,3 o 4 para SALIR, \n '1' (100 gr GALLETITAS CON MEMBRILLO/PEPAS $60), \n '2'(100 gr GALLETITAS CON CHIPS DE CHOCOLATE $80), \n '3' (100 gr GALLETITAS OREO $95), \n '4' SALIR"));
-    //cantidad_paquetes = parseInt(prompt("Ingrese cantidad de paquetes"));
+    marca_galletitas = parseInt(prompt("Ingrese categoría de galletitas 1,2,3 o 4 para SALIR, \n '1' (100 gr GALLETITAS CON MEMBRILLO/PEPAS $60) \n '2'(100 gr GALLETITAS CON CHIPS DE CHOCOLATE $80) \n '3' (100 gr GALLETITAS OREO $95) \n '4' Agregar al Carrito de Compras  \n '5' Ver Carrito de Compras  \n '6' SALIR"));
+    
 }
 alert("Gracias, lo esperamos pronto!")
